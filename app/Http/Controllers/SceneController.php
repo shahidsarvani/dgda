5c0667575b528aa09ef5daf7abe7375815f90efc
HamzaZafarBhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-06-14 17:12:36 +0500

    Update SceneController.php

--- app/Http/Controllers/SceneController.php
+++ app/Http/Controllers/SceneController.php
@@ -163,11 +163,11 @@ class SceneController extends Controller
         // create socket
         $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
         // bind socket to port
-        $result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
-        // start listening for connections
-        $result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
+        // $result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
+        // // start listening for connections
+        // $result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
 
-        return $result;
+        // return $result;
         // accept incoming connections
         // spawn another socket to handle communication
         $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
